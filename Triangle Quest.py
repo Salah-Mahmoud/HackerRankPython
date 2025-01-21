98c42384c80ea04e2246454f598db61bddc5f47a
Salah Mahmoud <124273567+Salah-Mahmoud@users.noreply.github.com>
2025-01-21 16:20:09 +0200

    Q 28

--- Triangle Quest.py
+++ Triangle Quest.py
@@ -1,6 +1,6 @@
-if __name__=="__main__":
+if __name__ == "__main__":
     n = int(input())
-    for i in range(1,n):
-        for x in range(1,i+1):
-            print(i,end = "")
+    for i in range(1, n):
+        for x in range(1, i + 1):
+            print(i, end="")
         print()
